c350b64384b9c5b23bf4126fa9191110df0f91db
Nicollas <nicollasramires@gmail.com>
2022-10-06 23:29:20 -0300

    added button

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { MagnifyingGlassPlus } from 'phosphor-react';
 import './styles/main.css';
 import logoImg from './assets/Logo.svg';
 
@@ -59,9 +60,16 @@ function App() {
 
 
       <div className='pt-1 bg-nlw-gradient self-stretch rounded-lg overflow-hidden mt-8'>
-        <div className='bg-[#2A2634] px-8 py-6'>
-          <strong className='text-2xl text-white font-black block'>Não encontrou seu duo?</strong>
-          <span className='text-zinc-400 block'>Publique um anúncio para encontrar novos players!</span>
+        <div className='bg-[#2A2634] px-8 py-6 flex justify-between items-center'>
+          <div>
+            <strong className='text-2xl text-white font-black block'>Não encontrou seu duo?</strong>
+            <span className='text-zinc-400 block'>Publique um anúncio para encontrar novos players!</span>
+          </div>
+
+          <button className='py-3 px-4 bg-violet-500 hover:bg-violet-700 text-white rounded flex items-center gap-3'>
+            <MagnifyingGlassPlus size={24}/>
+            Publicar Anúncio
+          </button>
         </div>
       </div>  
     </div>
